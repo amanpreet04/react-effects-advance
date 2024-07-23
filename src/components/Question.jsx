c88e2206b75af0029cb6186d2e98682727587536
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-23 09:54:26 +0530

    Added skip question functionality

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,15 +1,17 @@
 import questions from "../../questions";
 import Answer from "./Answer";
+import Skip from "./Skip";
 
 export default function Question({ questionId }) {
   const question = questions.find(
     (question) => question.id === questionId
   ).text;
   return (
-    <div id="question">
+    <div id="quiz">
       <>
         <h2>{question}</h2>
         <Answer questionId={questionId} />
+        <Skip label="Skip" />
       </>
     </div>
   );
