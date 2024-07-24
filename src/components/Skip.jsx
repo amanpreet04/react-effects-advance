f1026b221c834e8d8d58a1c75c66b1a156b8d915
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-24 22:46:55 +0530

    Consuming context in Skip

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { QuestionContext } from "../store/question-context-store";
+
 export default function Skip({ label }) {
+  const { addToViewedQuestions } = useContext(QuestionContext);
+
   return (
     <div id="skip-action">
       <button>{label}</button>
