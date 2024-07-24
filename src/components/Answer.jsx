ddf3846bfaa25e204c674b0fb0887f9b9853fdfb
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-24 22:45:03 +0530

    consuming context in Answer component

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import questions from "../../questions";
 import Options from "./Options";
+import { QuestionContext } from "../store/question-context-store";
 
-export default function Answer({ questionId }) {
+export default function Answer() {
+  const { currentQuestionId } = useContext(QuestionContext);
   const options = questions.find(
-    (question) => question.id === questionId
+    (question) => question.id === currentQuestionId
   ).answers;
   return (
     <div id="answers">
