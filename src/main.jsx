8e9e4864224d037356163ab128302d414f0ab9eb
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-25 23:03:13 +0530

    wrapping QuestionContextProvider around App

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import QuestionContextProvider from "./store/question-context-store.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QuestionContextProvider>
     <App />
-  </React.StrictMode>,
-)
+  </QuestionContextProvider>
+);
