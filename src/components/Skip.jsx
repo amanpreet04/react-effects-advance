0802aa8c217f93c9a1c50a24cf6d5503134ff4ad
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-26 22:05:53 +0530

    Handling skip operation

--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -2,17 +2,21 @@ import { useContext } from "react";
 import { QuestionContext } from "../store/question-context-store";
 
 export default function Skip({ label }) {
-  const { addToViewedQuestions } = useContext(QuestionContext);
+  const {
+    addToViewedQuestions,
+    currentQuestionId,
+    setCurrentQuestionId,
+    getNextQuestionId,
+  } = useContext(QuestionContext);
 
+  function handleSkipQuestions() {
+    addToViewedQuestions(currentQuestionId, "skipped");
+    setCurrentQuestionId(getNextQuestionId());
+  }
   return (
     <div id="skip-action">
-      <button
-        onClick={() => addToViewedQuestions(currentQuestionId, "skipped")}
-      >
-        {label}
-      </button>
+      <button onClick={() => handleSkipQuestions()}>{label}</button>
     </div>
   );
 }
 
-//onClick={}
