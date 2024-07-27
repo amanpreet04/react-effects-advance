18721054837c42b0d8bdd3dcbb45b32648931264
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-07-27 20:50:51 +0530

    Handling return in case of completed quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 import QUESTIONS from "../../questions";
-
+import quizCompletedImage from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionId = userAnswers.length;
 
+  const quizCompleted = activeQuestionId === QUESTIONS.length;
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   }
+
+  if (quizCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImage} alt="" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = QUESTIONS[activeQuestionId].answers;
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionId].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionId].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
